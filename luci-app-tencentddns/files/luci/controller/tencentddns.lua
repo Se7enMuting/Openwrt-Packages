c03768dd31f40fdba7c302b8b65f2c361dfc2e57
Se7enMuting <se7en@muting.cc>
2021-10-21 01:12:48 +0800

    up

--- luci-app-tencentddns/files/luci/controller/tencentddns.lua
+++
@@ -1,4 +0,0 @@
-module("luci.controller.tencentddns",package.seeall)
-function index()
-entry({"admin", "services", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),58).acl_depends = { "luci-app-tencentddns" }
-end
